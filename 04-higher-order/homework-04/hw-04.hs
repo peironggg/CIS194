457686a58df2437103ca4c5f24208e559e402ed4
peirong.wu <wupeirong294@gmail.com>
2020-12-14 09:26:15 +0800

    Fix indentation

--- 04-higher-order/homework-04/hw-04.hs
+++ 04-higher-order/homework-04/hw-04.hs
@@ -23,16 +23,17 @@ what they do.
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
-  | even x   = (x - 2) * fun1 xs
-  | otherwise = fun1 xs
+    | even x   = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
 fun1' = product . map (subtract 2) . filter even
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
-fun2 n | even n    = n + fun2 (n `div` 2)
-       | otherwise = fun2 (3 * n + 1)
+fun2 n 
+    | even n    = n + fun2 (n `div` 2)
+    | otherwise = fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (/=1) . iterate (\n -> if even n then n `div` 2 else 3 * n + 1)
@@ -86,7 +87,7 @@ correct computed height.
 -}
 --Exercise 2
 data Tree a = Leaf | Node Integer (Tree a) a (Tree a)
-  deriving (Show, Eq)
+    deriving (Show, Eq)
 
 foldTree :: [a] -> Tree a
 foldTree = foldr insert Leaf
