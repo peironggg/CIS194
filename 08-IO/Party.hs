4aefd01a170ae947c4a1a55fab29287fc67e67b1
peirong.wu <wupeirong294@gmail.com>
2021-01-05 13:48:50 +0800

    Update HW08 solutions after git checkout

--- 08-IO/Party.hs
+++ 08-IO/Party.hs
@@ -132,7 +132,7 @@ so you can convert it back into a Tree Employee using the read function
 -}
 --Exercise 4
 maxFun :: Tree Employee -> GuestList
-maxFun = uncurry moreFun . treeFold nextLevel mempty
+maxFun = uncurry moreFun . treeFold nextLevel
 
 {-
 Exercise 5
@@ -162,17 +162,4 @@ main = readFile "company.txt" >>= putStrLn . getRes . read
     format (GL emps funScore) = "Total fun: " ++ show funScore ++ "\n" ++ empNames emps
       where
         empNames :: [Employee] -> String
-        empNames = unlines . map empNameat :: GuestList -> String
-    format (GL emps funScore) = "Total fun: " ++ show funScore ++ "\n" ++ empNames emps
-      where
-        empNames :: [Employee] -> String
         empNames = unlines . map empName
-
-
-
-
-
-
-
-
-
